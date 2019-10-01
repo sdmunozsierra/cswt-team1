dd320c84774ae0ff408f63edae67fa5f144ef741
Mark Williams <43056671+mawilliams7@users.noreply.github.com>
2019-10-01 08:05:17 -0600

    Add files via upload

--- cswt/src/client/Driver.java
+++ cswt/src/client/Driver.java
@@ -4,7 +4,7 @@ import cswt.Ticket;
 
 public class Driver {
     public static void main(String [] args) {
-    	JSONPacketClient client = new JSONPacketClient();
+    	ClientHandler client = new ClientHandler();
     	client.createTicket("title", "description", "client", "severity");
     	for (Ticket ticket: client.getAllTickets()) {
     		System.out.println(ticket.getDescription());
