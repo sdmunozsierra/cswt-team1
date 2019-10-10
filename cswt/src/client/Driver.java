62def9af03801d9928d19ef4b941ff4716d83ba2
Mark Williams <43056671+mawilliams7@users.noreply.github.com>
2019-10-10 10:48:16 -0600

    Add files via upload

--- cswt/src/client/Driver.java
+++ cswt/src/client/Driver.java
@@ -5,9 +5,28 @@ import cswt.Ticket;
 public class Driver {
     public static void main(String [] args) {
     	ClientHandler client = new ClientHandler();
-    	client.createTicket(new Ticket());
-    	for (Ticket ticket: client.getAllTickets()) {
-    		System.out.println(ticket.getDescription());
+    	Ticket ticket = new Ticket();
+    	ticket.setTitle("Hello");
+    	ticket.setDescription("here");
+    	ticket.setClient("k");
+    	ticket.setSeverity("i");
+    	ticket.setPriority("1");
+    	ticket.setAssignedTo("g");
+    	client.createTicket(ticket);
+    	String id = "";
+    	for (Ticket t: client.getAllTickets()) {
+    		System.out.println(t.getTitle());
+    		id = t.getId();
     	}
+        Ticket ticket2 = new Ticket();
+    	ticket2.setId(id);
+        ticket2.setTitle("DED");
+        ticket2.setDescription("here");
+        ticket2.setClient("k");
+        ticket2.setSeverity("i");
+        ticket2.setPriority("1");
+        ticket2.setAssignedTo("g");
+        client.editTicket(ticket2);
+		System.out.println(client.getTicket(ticket2.getId()).getTitle());
     }
 }
