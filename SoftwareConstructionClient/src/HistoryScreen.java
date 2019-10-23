35171e9dce771517f59891ac7ef175f0f0547eca
marknunez97 <marknunez1997@gmail.com>
2019-10-23 10:26:12 -0600

    working on history popup

--- SoftwareConstructionClient/src/HistoryScreen.java
+++ SoftwareConstructionClient/src/HistoryScreen.java
@@ -5,5 +5,22 @@ public class HistoryScreen {
     public JPanel historyViewManagement;
     public JComboBox filter;
     public JList historyList;
-
+/*
+    Create Ticket
+    Open Ticket
+    Close Ticket
+    Mark Ticket as fixed
+    Reject Ticket
+    Edit Ticket
+    Search Ticket
+    Update All Tickets
+    Get Ticket
+    Create Account
+    Validate User
+    Edit User
+    Delete User
+    Update All Users
+    Get All Users
+    Get User
+*/
 }
