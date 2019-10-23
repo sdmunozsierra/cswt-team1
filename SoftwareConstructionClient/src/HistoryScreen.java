8e3cb279ed28b6505fb89070408af3c7f2b1a068
Luis Gutierrez <luisgtzh18@gmail.com>
2019-10-22 18:13:44 -0600

    Added HistoryScreen files

---
+++ SoftwareConstructionClient/src/HistoryScreen.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class HistoryScreen {
+    public JPanel mainScreen;
+    public JPanel historyViewManagement;
+    public JComboBox filter;
+    public JList historyList;
+
+}
